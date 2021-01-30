97e91c57d4b0bb765fea3069974ca2d89fdf0cbd
ItzelVigil <itzel.vigilch@gmail.com>
2021-01-30 12:53:53 -0700

    cm2

--- perceptron.py
+++ perceptron.py
@@ -20,7 +20,7 @@ class Perceptron:
     def prediccion(self,entrada1, entrada2):
         bias = self.weights[0]
         activation = (entrada1 * self.weights[1]) + (entrada2 * self.weights[2]) + bias
-        return 1 if activation > 0 else -1
+        return 1 if activation > 0 else 0
 
     def ajustarpesos(self,valorReal, rPred, e1, e2):
         self.weights[0] = self.weights[0] + valorReal
@@ -40,6 +40,20 @@ class Perceptron:
                     print('Adjusted weights: {}'.format(self.weights))
             print('Final weights from epoch {}: {}'.format(epoch,self.weights))
 
+    def verify(self):
+        count = 0
+        for i in range(0,4):
+            valorReal = self.df.iloc[i]['r']
+            e1 = self.df.iloc[i]['x']
+            e2 = self.df.iloc[i]['y']
+            resultadoPrediccion = self.prediccion(e1,e2)
+            if resultadoPrediccion != valorReal:
+                count = count + 1
+        return (1-count/len(self.df))*100
+
 perceptron = Perceptron(3)
-perceptron.entrenamiento(epochs=5)
-print('\nFinal weights from training: {}'.format(perceptron.weights))
+perceptron.entrenamiento(epochs=4)
+
+print('\nPesos finales: {}'.format(perceptron.weights))
+accuracy = perceptron.verify()
+print('Error: {} %'.format(100-accuracy))
